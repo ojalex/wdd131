408f342d91cdb4703a1f45b2bc57f26c3489dcf9
ojalex <jumaalex448@gmail.com>
2024-11-08 02:21:19 +0300

    Adjusted the layout

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = "Last Modification:" + text;
+
+document.getElementById("currentyear").innerHTML = "&copy2024 OJ Alex Otieno Juma, Kenya"
