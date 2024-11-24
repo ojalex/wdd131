061335e9adcc50a847a48215a8cb127995efc5bb
ojalex <jumaalex448@gmail.com>
2024-11-24 21:08:46 +0300

    Place Page

---
+++ scripts/place.js
@@ -0,0 +1,3 @@
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = "Last Modification:" + text;
+document.getElementById("currentyear").innerHTML = "&copy2024 OJ Alex Otieno Juma, Kenya"
